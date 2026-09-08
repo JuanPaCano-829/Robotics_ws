2bbf49098cf8ca812b86c0e74e7d61d5bd83fdfd
JuanPaCano-829 <juanpa2_canorub@outlook.com>
2026-09-07 21:43:37 -0600

    workspace y velocity_publisher de clase

---
+++ src/basics/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'basics'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pablo-cano',
+    maintainer_email='juanpa2_canorub@outlook.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+	'velocity_publisher = basics.velocity_publisher:main',
+	'velocity_subscriber = basics.velocity_subscriber:main'
+        ],
+    },
+)
